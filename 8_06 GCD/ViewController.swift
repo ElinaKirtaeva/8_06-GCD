920eb4cf60ebaa2cfef6dbd0470108c8f38ea795
Elina Kirtaeva <elin-ru1@yandex.ru>
2022-01-31 15:39:33 +0300

    Initial Commit

---
+++ 8_06 GCD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  8_06 GCD
+//
+//  Created by Элина Рупова on 31.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
